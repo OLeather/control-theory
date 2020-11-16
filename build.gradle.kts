e0c7d6e0f285341d612c04b94c95db702e21023d
Owen Leather <43189206+OLeather@users.noreply.github.com>
2020-11-16 13:00:22 -0800

    Reformat code

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,24 @@ plugins {
 
 
 allprojects {
+    apply(plugin = "java")
+    apply(plugin = "kotlin")
+
     group = "org.example"
     version = "1.0"
 
+    repositories {
+        mavenCentral()
+        maven(url = "https://dl.bintray.com/kyonifer/maven")
+        jcenter()
+    }
+
+    dependencies {
+        add("implementation", "org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        add("testCompile", "junit:junit:4.13")
+        compile("com.kyonifer", "koma-core-ejml", "0.12")
+        compile("com.kyonifer", "koma-plotting", "0.12")
+    }
 
     tasks.withType<JavaCompile> {
         sourceCompatibility = "1.8"
@@ -22,22 +37,5 @@ allprojects {
             freeCompilerArgs = listOf("-Xjsr305=strict")
             jvmTarget = "1.8"
         }
-    }
-}
-
-allprojects{
-    apply(plugin = "java")
-    apply(plugin = "kotlin")
-    repositories {
-        mavenCentral()
-        maven(url="https://dl.bintray.com/kyonifer/maven")
-        jcenter()
-    }
-
-    dependencies {
-        add("implementation", "org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-        add("testCompile", "junit:junit:4.13")
-        compile("com.kyonifer", "koma-core-ejml", "0.12")
-        compile("com.kyonifer", "koma-plotting", "0.12")
     }
 }
